53b465710edcf29f9a3484541da8729a674692e0
Petr Maliarov <petr.maliarov@gmail.com>
2018-09-18 21:45:33 +0300

    Update sys_setup.sh

--- setup/sys_setup.sh
+++ setup/sys_setup.sh
@@ -1,12 +1,4 @@
-# Network setup
-ip link set lo up
-ip link set eno1 up
-cp /etc/netctl/examples/ethernet-dhcp /etc/netctl/ethernet-dhcp
-sed -i 's/eth0/eno1/g' /etc/netctl/ethernet-dhcp
-netctl enable ethernet-dhcp
-reboot
-
-# Update systed
+# Update system
 pacman -Syu --noconfirm
 
 # Install video drivers
