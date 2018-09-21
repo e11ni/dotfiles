fe9a4836a2c76f52ac248f8a6ad6d9d311d80b83
Petr Maliarov <petr.maliarov@gmail.com>
2018-09-21 09:35:59 +0300

    update congig

--- setup/sys_setup.sh
+++ setup/sys_setup.sh
@@ -6,7 +6,7 @@ pacman -S --noconfirm mesa
 pacman -S --noconfirm xf86-video-intel
 pacman -S --noconfirm vulkan-intel
 
-# Install latest kernel
+# Install latest kernel and update bootloader
 pacman -S --noconfirm linux-lts
 pacman -S --noconfirm linux-lts-headers
 
@@ -14,6 +14,68 @@ cp /boot/loader/entries/arch.conf /boot/loader/entries/arch-lts.conf
 sed -i 's/-linux/-linux-lts/g' /boot/loader/entries/arch-lts.conf
 sed -i 's/arch/arch-lts/g' /boot/loader/loader.conf
 
+# Install vim
+pacman -S --noconfirm vim
+
+# Add user
+useradd petrmali
+passwd petrmali
+mkhomedir_helper petrmali
+usermod -aG wheel petrmali
+
+# Allow users from wheel group run commands using sudo
+sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /etc/sudoers
+
 # Install desktop environment 
 pacman -S --noconfirm sway
 
+# Install some X utils
+pacman -Fy
+pacman-key --refresh-keys
+pacman -S --noconfirm vlc libreoffice p7zip unrar tar rsync pinta conky
+pacman -S --noconfirm termite rofi
+yay -S --noconfirm ttf-iosevka-pack stacer
+
+# Install yay - aur helper written on Go
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+cd ..
+rm -rf yay
+sudo ln -s /usr/bin/yay /usr/bin/yaourt
+
+# Install zsh
+pacman -S --noconfirm  zsh
+zsh
+chsh -s /bin/zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo pacman -S --noconfirm powerline-fonts
+sudo pacman -S --noconfirm zsh-completions
+
+# Install cmd utils
+sudo pacman -S --noconfirm  fzf fasd the_silver_searcher npm pandoc trash-cli jq httpie hub
+sudo npm install -g tldr how-2
+yay -S --noconfirm yadm-git lf-git lnav-git doctoc pet-git ctop
+go get -u github.com/nishanths/license
+
+# Install X utils
+sudo pacman -S --noconfirm meld code wireshark-qt
+yay -S --noconfirm copyq
+
+# Config git
+git config --global user.name "Petr Maliarov"
+git config --global user.email "petr.maliarov@gmail.com"
+
+# Install and config tmux
+sudo pacman -S --noconfirm tmux tmuxp
+mkdir ~/.tmux && mkdir ~/.tmux/plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Install internet apps
+sudo pacman -S --noconfirm min telegram-desktop
+yay -S --noconfirm google-chrome whatsapp-web-desktop slack
+
+# Install docker
+sudo pacman -S --noconfirm docker docker-compose
+sudo usermod -aG wheel petrmali
+sudo systemctl enable docker
